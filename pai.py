b4ef02102be8f9533554d19bd3b593275cb166e8
Redstoners <robinlego@hotmail.nl>
2017-01-28 10:43:54 +0100

    Update 1.0

--- pai.py
+++ pai.py
@@ -2,16 +2,19 @@
 import time
 
 
+# Makes the pause command
 def pause(seconds):
     time.sleep(seconds)
 
-
+# Makes the speak command
 def speak(text):
     print(text)
 
+# Initializes the time and date
 time.ctime()
 current_time = time.strftime('%H:%M')
 current_date = time.strftime('%d %B %Y')
+
 
 speak("Hello! i am PAI, your owm personal assistant")
 
@@ -19,7 +22,12 @@ speak("Hello! i am PAI, your owm personal assistant")
 var = 1
 while var == 1:
     input = raw_input(">>>")
-    if input == "time":
+    if input == "help":
+        file = open('bin/commands.txt', 'r')
+        file_contents = file.read()
+        print (file_contents)
+        file.close()
+    elif input == "time":
         speak("The current time is " + current_time)
     elif input == "hello":
         print("Hello human!")
